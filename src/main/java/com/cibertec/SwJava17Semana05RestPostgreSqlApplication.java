6e04eb937377c19787a264b4518e425c30468755
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-10-17 19:26:05 -0500

    inicio

---
+++ src/main/java/com/cibertec/SwJava17Semana05RestPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwJava17Semana05RestPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwJava17Semana05RestPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/SwJava17Semana05RestPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwJava17Semana05RestPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
